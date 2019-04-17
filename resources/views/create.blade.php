fa02956eff9bc3fd9e3a5e6e2e04a66e21523491
Joseph Herbert Tandingan <jh.tandingan@coi.local>
2019-04-17 11:05:50 +0800

    Create view (create)

---
+++ resources/views/create.blade.php
@@ -0,0 +1,41 @@
+@extends('layout')
+
+@section('content')
+    <style>
+        .uper {
+            margin-top: 40px;
+        }
+    </style>
+    <div class="card uper">
+        <div class="card-header">
+            Add Book
+        </div>
+        <div class="card-body">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ error }}</li>
+                        @endforeach
+                    </ul>
+                </div><br>
+            @endif
+            <form method="POST" action="{{ route('books.store') }}">
+                @csrf
+                <div class="form-group">
+                    <label for="name">Book Name:</label>
+                    <input type="text" class="form-control" name="book_name">
+                </div>
+                <div class="form-group">
+                    <label for="isbn_no">Book ISBN Number:</label>
+                    <input type="text" class="form-control" name="isbn_no">
+                </div>
+                <div class="form-group">
+                    <label for="price">Book Price:</label>
+                    <input type="text" class="form-control" name="price">
+                </div>
+                <button type="submit" class="btn btn-primary">Create Book</button>
+            </form>
+        </div>
+    </div>
+@endsection
